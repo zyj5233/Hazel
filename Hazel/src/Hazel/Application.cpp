96708ad01e61d15aeaf30817a9c5345fd8b27aa6
牧之原箱子 <1691002987@qq.com>
2025-04-29 13:20:03 +0800

    第一个三角形

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -21,6 +21,33 @@ namespace Hazel {
 
         m_ImGuiLayer = new ImGuiLayer();
         PushOverlay(m_ImGuiLayer);
+
+        glGenVertexArrays(1, &m_VertexArray);       //创建vao，绘图配方
+        glBindVertexArray(m_VertexArray);           //绑定为当前操作对象
+
+        glGenBuffers(1, &m_VertexBuffer);           //创建vbo，原料仓库
+        glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);  //绑定到GL_ARRAY_BUFFER
+
+        float vertices[3 * 3] = {
+            -1.0f, 0.0f, 0.0f,     //左下角（x，y，z）
+             1.0f, 0.0f, 0.0f,     //右下角
+             0.0f,  0.5f, 0.0f      //顶点
+        };
+        
+        //数据传到vbo
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);      //静态数据静态绘制
+
+        //取数据
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        glEnableVertexAttribArray(0);
+
+        //ibo索引
+        glGenBuffers(1, &m_IndexBuffer);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
+
+        //索引数组和传递索引数组
+        unsigned int indices[3] = { 0, 1, 2 };
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     }
 
 
@@ -57,8 +84,12 @@ namespace Hazel {
     {
         while (m_Running)
         {
-            glClearColor(1, 1, 1, 1);
+            glClearColor(0.1f, 0.1f, 0.1f, 1);       //窗口颜色
             glClear(GL_COLOR_BUFFER_BIT);
+
+            //方便多个vao切换
+            glBindVertexArray(m_VertexArray);
+            glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);      //绘制三角形，3个索引数量，索引int型
 
             for (Layer* layer : m_LayerStack)
                 layer->OnUpdate();      //是层级的onupdata，处理onupdata
--- Hazel/src/Platform/OpenGL/OpenGLContext.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLContext.cpp
@@ -10,14 +10,21 @@ namespace Hazel {
     OpenGLContext::OpenGLContext(GLFWwindow* windowHandle)
         : m_WindowHandle(windowHandle)
     {
-        HZ_CORE_ASSERT(windowHandle, "Window handle is null!")
+        HZ_CORE_ASSERT(windowHandle, "Window handle is null!")      //检查wondowHandel是否为空
     }
 
     void OpenGLContext::Init()
     {
         glfwMakeContextCurrent(m_WindowHandle);
-        int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+        int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);        //获取OpengGL函数地址
         HZ_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+        if (status) {
+            HZ_CORE_INFO("OpenGL Info:");
+            HZ_CORE_INFO("  Vendor: {0}", reinterpret_cast<const char*>(glGetString(GL_VENDOR)));       //类型转换，不然会报错
+            HZ_CORE_INFO("  显卡: {0}", reinterpret_cast<const char*>(glGetString(GL_RENDERER)));
+            HZ_CORE_INFO("  版本: {0}", reinterpret_cast<const char*>(glGetString(GL_VERSION)));
+        }
     }
 
     void OpenGLContext::SwapBuffers()
